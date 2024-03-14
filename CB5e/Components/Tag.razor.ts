0a9f3b5b389d7b1c57282d0693f9a5c08516a413
Maluku <Maluku@SEITO7>
2024-03-14 05:18:38 +0100

    Switched to Bootstrap popovers instead of iframes

--- CB5e/Components/Tag.razor.ts
+++ CB5e/Components/Tag.razor.ts
@@ -1,3 +1,5 @@
+declare var bootstrap: any;
+
 export function Display(data: string, stylesheet: string, x: number, y: number) {
     const xsltProcessor = new XSLTProcessor();
     const myXMLHTTPRequest = new XMLHttpRequest();
@@ -24,6 +26,51 @@ export function Display(data: string, stylesheet: string, x: number, y: number) 
     myXMLHTTPRequest.send(null);
 }
 
+function setContent(popup, reference) {
+    const xsltProcessor = new XSLTProcessor();
+    const myXMLHTTPRequest = new XMLHttpRequest();
+    var result = "Loading...";
+    myXMLHTTPRequest.onload = function () {
+        const xslRef = myXMLHTTPRequest.responseXML;
+
+        // Finally import the .xsl
+        xsltProcessor.importStylesheet(xslRef);
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(reference.invokeMethod('GetContent'), "text/xml");
+        const resultDoc = xsltProcessor.transformToDocument(doc);
+
+        //reference.invokeMethod('GetPopup').setContent(new XMLSerializer().serializeToString(resultDoc));
+        result = new XMLSerializer().serializeToString(resultDoc);
+    }
+    myXMLHTTPRequest.open("GET", "xsl/" + reference.invokeMethod('GetStylesheet') + ".xsl", false);
+    myXMLHTTPRequest.send(null);
+    return result;
+}
+
 function Hide() {
     document.getElementById("popup").style.display = "none";
 }
+
+
+export function bootstrapPopover(element, reference) {
+    var popup = new bootstrap.Popover(element, {
+        "content": () => setContent(element, reference),
+        "sanitize": true,
+        "template": '<div class="popover" role="tooltip"><div class="popover-close float-end"><div class="btn-close me-1 mt-1" aria-label="Close"></div></div><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    });
+    element.onclick = function (event) {
+        event.stopPropagation();
+    };
+    element.addEventListener('inserted.bs.popover', () => {
+        if (popup.tip) {
+            popup.tip.getElementsByClassName("btn-close")[0].onclick = function (e) {
+                popup.hide();
+            }
+        }
+    });
+    return popup;
+}
+
+const myDefaultAllowList = bootstrap.Tooltip.Default.allowList;
+myDefaultAllowList.center = [];
+myDefaultAllowList['*'].push('style');
